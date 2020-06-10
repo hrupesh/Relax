3b0ff1a742ecff1818a13e252fabdc9db3ee2a76
hrupesh <rcashok0@gmail.com>
2020-06-10 21:05:52 +0530

    V 1.0

--- js/app.js
+++ js/app.js
@@ -1,31 +1,45 @@
-const container =  document.querySelector('.container');
-const text =  document.querySelector('#text');
+const container = document.querySelector(".container");
+const text = document.querySelector("#text");
+const pointer = document.querySelector(".pointer");
+const circle = document.querySelector(".circle");
 
-const totalTime  = 6000
-const breathTime = (totalTime / 5) * 2
-const holdTime = totalTime / 5
+var audio = new Audio("../assets/om-edited.mp3");
+audio.loop = true;
+audio.play();
 
-breathAnimation()
+const totalTime = 6000;
+const breathTime = (totalTime / 5) * 2;
+const holdTime = totalTime / 5;
 
-function breathAnimation(){
-    text.innerText = 'Breath In.........'
-    // console.log('Breath In.........');
-    // document.body.style.background = 'linear-gradient(135deg,red 5%,green 75%)';
-    container.className = 'container grow'
-    
+breathAnimation();
+
+function breathAnimation() {
+  text.innerText = "Breath In!";
+  // console.log('Breath In.........');
+  // document.body.style.background = 'linear-gradient(135deg,red 5%,green 75%)';
+  container.className = "container grow";
+  pointer.style.backgroundColor = "rgb(250, 108, 108";
+  circle.style.backgroundColor = "#21120f";
+
+  setTimeout(() => {
+    text.innerText = "Hold!";
+    pointer.style.backgroundColor = "rgb(243, 233, 100";
+    circle.style.backgroundColor = "#252412";
+    // console.log('Hoollllllld!');
+    // document.body.style.background = 'linear-gradient(135deg,green 5%,red 75%)';
+
     setTimeout(() => {
-        text.innerText = 'Hoolllllld!'
-        // console.log('Hoollllllld!');
-        // document.body.style.background = 'linear-gradient(135deg,green 5%,red 75%)';
-        
-        setTimeout(() => {
-            text.innerText = 'Breathe Out.........'
-            // console.log('Breathe Out........');
-            // document.body.style.background = 'linear-gradient(135deg,blue 5%,purple 75%)';
-            container.className = 'container shrink'
-        }, holdTime)
-
-    },breathTime)
+      text.innerText = "Breath Out!";
+      pointer.style.backgroundColor = "rgb(149, 207, 82)";
+      circle.style.backgroundColor = "#101d0f";
+      // console.log('Breathe Out........');
+      // document.body.style.background = 'linear-gradient(135deg,blue 5%,purple 75%)';
+      container.className = "container shrink";
+    }, holdTime);
+  }, breathTime);
 }
 
-setInterval(breathAnimation,totalTime)
+setInterval(breathAnimation, totalTime);
+
+window.addEventListener("online", () => window.alert("Now You can experience this app at it's Best 👍"));
+window.addEventListener("offline", () => window.alert("For full immersive experience use with an internet connection ⚡"));
